64e004bb6825ea161dc4a4bfb42f7a34b7598386
towfique <trrabby1@gmail.com>
2025-01-14 20:07:33 +0600

    added name in jwtPayload

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -40,6 +40,7 @@ const loginUser = async (payload: TLoginUser) => {
   const jwtPayload = {
     email: user.email,
     role: user.role,
+    name: user.name,
   };
 
   const accessToken = createToken(
